f94e0aaae20a8b08bf29f4a9d7c488fdd0ddd8d8
Juliana dos Santos Freitas <jujugemeasf@gmail.com>
2020-05-26 14:02:04 -0300

    complete exercises

--- script_2.js
+++ script_2.js
@@ -5,7 +5,7 @@
 //Retorne o seguinte conteudo:
 //O usuário mora em São Paulo / SP,no bairro Centro, na rua "Rua dos Pinheiros" com n° 1293.
 
-/*var endereco = {
+var endereco = {
     rua: "Rua dos Pinheiros",
     numero: 1293,
     bairro: "Centro",
@@ -14,12 +14,12 @@
 }
 
 console.log(`O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua ${endereco.rua} com n° ${endereco.numero}.`)
-*/
+
 //EXERCÍCIO 2
 
 //crie uma função que dado um intervalo (entre x e y) exiba todos os números pares
 
-/*function pares(x,y){
+function pares(x,y){
     while(x<=y){
         if((x%2)==0){
         var par = x
@@ -30,13 +30,13 @@ console.log(`O usuário mora em ${endereco.cidade} / ${endereco.uf}, no bairro $
 }
 
 pares(32, 321);
-*/
+
 //EXERCICIO 3 
 
-/*Escreva uma função que verifique se o vetor de habilidade passado possui a habilidade "javascript" e retorna um boleano true/false caso
-exista ou não*/
+//Escreva uma função que verifique se o vetor de habilidade passado possui a habilidade "javascript" e retorna um boleano true/false caso
+//exista ou não
 
-/*function temHabilidade(skills){
+function temHabilidade(skills){
     if(skills.indexOf("Javascript") ==  0){
         return true
     } else {
@@ -46,7 +46,7 @@ exista ou não*/
 
 var skills = [ "Javascript", "ReactJS", "React Native"];
 temHabilidade(true || false);
-*/
+
 // EXERCICIO 4
 
 //Escreva uma função que dado um total de anos de estudo retorna o quão experiente o usuário é
@@ -79,3 +79,21 @@ experiencia(anosEstudos);
 //De 7 anos acima: Jedi Master
 
 //EXERCICIO 5
+
+// Escreva uma função que produza o seguinte resultado: O Diego possui as habilidades: Javascript, ReactJS, Redux
+//O Gabriel possui as habilidades: VueJS, Ruby on Rails, Elixir
+
+var usuarios = [
+    {
+        nome: "Diego",
+        habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+        nome: "Gabriel",
+        habilidades: ["Vue", "Ruby on Rails", "Elixir"]
+    }
+];
+
+for (let usuario of usuarios) {
+       console.log(`O ${usuario.nome} possui as habilidades: ${usuario.habilidades.join()}.`)
+}
